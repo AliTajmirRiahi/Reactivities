f8454f2e2a8e78ca994bb8de43826b7e312b987b
Tajmir <Tajmir@Tajmir>
2020-04-04 16:53:48 +0430

    End of Section 6

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect, Fragment } from 'react';
 import { Container } from 'semantic-ui-react';
-import axios from 'axios';
 import NavBar from '../../features/nav/NavBar';
 import { IActivity } from '../models/activity';
 import AvtivityDashboard from '../../features/activities/dashboard/AvtivityDashboard';
+import agent from '../api/agent';
+import Loading from './Loading';
 
 const App = () => {
   const [activities, setActivities] = useState<IActivity[]>([]);
@@ -11,21 +12,15 @@ const App = () => {
     null
   );
   const [editMode, setEditMode] = useState(false);
+  const [loading, setLoading] = useState(true);
+  const [submitting, setSubmitting] = useState(false);
 
   const handleSelectActivity = (id: string) => {
-    setSelectActivity(activities.filter(ac => ac.id === id)[0]);
+    setSelectActivity(activities.filter((ac) => ac.id === id)[0]);
     setEditMode(false);
   };
   const handleNewActivity = () => {
-    setSelectActivity({
-      id: '',
-      title: '',
-      description: '',
-      category: '',
-      date: '',
-      city: '',
-      venue: ''
-    });
+    setSelectActivity(null);
     setEditMode(true);
   };
 
@@ -34,32 +29,45 @@ const App = () => {
   }, []);
 
   const getActivities = async () => {
-    const res = await axios.get<IActivity[]>('/api/activities');
-    let activities = res.data;
-    activities.forEach(ac => {
-      ac.date = ac.date.split('.')[0];
-    });
-    setActivities(activities);
+    agent.Activities.list()
+      .then((response) => {
+        let activities: IActivity[] = [];
+        response.forEach((ac: any) => {
+          ac.date = ac.date.split('.')[0];
+          activities.push(ac);
+        });
+        setActivities(activities);
+      })
+      .then(() => setLoading(false));
   };
 
   const handleCreateActivity = (activity: IActivity) => {
-    setActivities([...activities, activity]);
-    setSelectActivity(activity);
-    setEditMode(false);
+    setSubmitting(true);
+    agent.Activities.create(activity)
+      .then(() => {
+        setActivities([...activities, activity]);
+        setSelectActivity(activity);
+        setEditMode(false);
+      })
+      .then(() => setSubmitting(false));
   };
   const handleEditActivity = (activity: IActivity) => {
-    setActivities(
-      activities.map(ac => (ac.id === activity.id ? activity : ac))
-    );
-    setSelectActivity(activity);
-    setEditMode(false);
+    agent.Activities.update(activity).then(() => {
+      setActivities(
+        activities.map((ac) => (ac.id === activity.id ? activity : ac))
+      );
+      setSelectActivity(activity);
+      setEditMode(false);
+    });
   };
 
   const handleDeleteActivity = (id: string) => {
-    setActivities(activities.filter(ac => ac.id !== id));
+    agent.Activities.delete(id).then(() => {
+      setActivities(activities.filter((ac) => ac.id !== id));
+    });
   };
 
-  if (activities === null) return <Fragment></Fragment>;
+  if (loading && activities === null) return <Loading content='Loading ...' />;
   return (
     <Fragment>
       <NavBar newActivity={handleNewActivity} />
